1dc5c126b45e95adb3ff8a5e1e1f558f6c853a3b
Guido Santillan <guidosantillan11@gmail.com>
2025-04-15 22:09:25 -0300

    primer commit

---
+++ tareas/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { BienvenidaComponent } from './bienvenida/bienvenida.component';
+import { ErrorComponent } from './error/error.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'bienvenida', component: BienvenidaComponent },
+  { path: '**', component: ErrorComponent } 
+];
